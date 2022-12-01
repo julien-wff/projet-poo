06765281ecdc987d57675faa718377a4fa2324d6
Rooomain <rommor2810@gmail.com>
2022-12-01 16:18:34 +0100

    feat: orderentity

---
+++ Components/enities/OrderEntity.cpp
@@ -0,0 +1,64 @@
+﻿#include "pch.h"
+#include "OrderEntity.h"
+
+Entities::OrderEntity::OrderEntity()
+{
+}
+
+Entities::OrderEntity::OrderEntity(DateTime deliveryDate, DateTime creationDate, int clientId) :
+    DeliveryDate(deliveryDate), CreationDate(creationDate), ClientId(clientId)
+{
+}
+
+Entities::OrderEntity::OrderEntity(String^ orderReference, DateTime deliveryDate, DateTime creationDate, int clientId) :
+    OrderReference(orderReference), DeliveryDate(deliveryDate), CreationDate(creationDate), ClientId(clientId)
+{
+}
+
+Entities::OrderEntity::OrderEntity(DataRow^ row)
+{
+    this->ClientId = Convert::ToInt32(row["client_id"]);
+    this->OrderReference = Convert::ToString(row["reference"]);
+    this->DeliveryDate = Convert::ToDateTime(row["delivery_date"]);
+    this->CreationDate = Convert::ToDateTime(row["creation_date"]);
+}
+
+String^ Entities::OrderEntity::GetOrderReference()
+{
+    return OrderReference;
+}
+
+DateTime Entities::OrderEntity::GetDeliveryDate()
+{
+    return DeliveryDate;
+}
+
+DateTime Entities::OrderEntity::GetCreationDate()
+{
+    return CreationDate;
+}
+
+int Entities::OrderEntity::GetClientId()
+{
+    return ClientId;
+}
+
+void Entities::OrderEntity::SetOrderReference(String^ orderReference)
+{
+    OrderReference = orderReference;
+}
+
+void Entities::OrderEntity::SetDeliveryDate(DateTime deliveryDate)
+{
+    DeliveryDate = deliveryDate;
+}
+
+void Entities::OrderEntity::SetCreationDate(DateTime creationDate)
+{
+    CreationDate = creationDate;
+}
+
+void Entities::OrderEntity::SetClientId(int clientId)
+{
+    ClientId = clientId;
+}
---
+++ Components/enities/OrderEntity.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+using namespace System;
+using namespace Data;
+
+namespace Entities
+{
+    public ref class OrderEntity
+    {
+    protected:
+        String^ OrderReference;
+        DateTime DeliveryDate;
+        DateTime CreationDate;
+        int ClientId;
+    public:
+        OrderEntity();
+        OrderEntity(DateTime deliveryDate, DateTime creationDate, int clientId);
+        OrderEntity(String^ orderReference, DateTime deliveryDate, DateTime creationDate, int clientId);
+        OrderEntity(DataRow^ row);
+        String^ GetOrderReference();
+        DateTime GetDeliveryDate();
+        DateTime GetCreationDate();
+        int GetClientId();
+        void SetOrderReference(String^ orderReference);
+        void SetDeliveryDate(DateTime deliveryDate);
+        void SetCreationDate(DateTime creationDate);
+        void SetClientId(int clientId);
+    };
+}
