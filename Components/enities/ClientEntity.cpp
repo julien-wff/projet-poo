9065693ca731d3409203ee85547ad6c34ff308ef
Antonin B <antonin.bourrel@viacesi.fr>
2022-12-01 14:26:36 +0100

    feat: client entity

---
+++ Components/enities/ClientEntity.cpp
@@ -0,0 +1,42 @@
+#include "pch.h"
+#include "ClientEntity.h"
+
+Entities::ClientEntity::ClientEntity()
+{
+}
+
+Entities::ClientEntity::ClientEntity(Entities::PersonEntity^ person, DateTime birthDate)
+    : Entities::PersonEntity(person), BirthDate(birthDate)
+{
+}
+
+Entities::ClientEntity::ClientEntity(int clientId, Entities::PersonEntity^ person, DateTime birthDate) :
+    ClientId(clientId), Entities::PersonEntity(person), BirthDate(birthDate)
+{
+}
+
+Entities::ClientEntity::ClientEntity(DataRow^ row)
+{
+    this->ClientId = Convert::ToInt32(row["id"]);
+    this->BirthDate = Convert::ToDateTime(row["birth_date"]);
+}
+
+int Entities::ClientEntity::GetClientId()
+{
+    return ClientId;
+}
+
+DateTime Entities::ClientEntity::GetClientBirthDate()
+{
+    return BirthDate;
+}
+
+void Entities::ClientEntity::SetClientId(int clientId)
+{
+    ClientId = clientId;
+}
+
+void Entities::ClientEntity::SetBirthDate(DateTime birthDate)
+{
+    BirthDate = birthDate;
+}
