6ea4fb0c753c39be1bc6ab326ef4421d196a61df
Rooomain <rommor2810@gmail.com>
2022-12-02 09:29:24 +0100

    fix: paymententity

---
+++ Components/enities/PaymentEntity.cpp
@@ -0,0 +1,85 @@
+﻿#include "pch.h"
+#include "PaymentEntity.h"
+
+Entities::PaymentEntity::PaymentEntity()
+{
+}
+
+Entities::PaymentEntity::PaymentEntity(int paymentModeid, String^ orderReference, float amount, DateTime paymentDate,
+                                       DateTime expectedDate): PaymentModeId(paymentModeid),
+                                                               OrderReference(orderReference), Amount(amount),
+                                                               PaymentDate(paymentDate), ExpectedDate(expectedDate)
+{
+}
+
+Entities::PaymentEntity::PaymentEntity(int paymentId, int paymentModeid, String^ orderReference, float amount,
+DateTime paymentDate, DateTime expectedDate): PaymentId(paymentId), PaymentModeId(paymentModeid),
+                        OrderReference(orderReference), Amount(amount),
+                        PaymentDate(paymentDate), ExpectedDate(expectedDate)
+{
+}
+
+Entities::PaymentEntity::PaymentEntity(DataRow^ row)
+{
+}
+
+int Entities::PaymentEntity::GetpaymentId()
+{
+    return PaymentId;
+}
+
+int Entities::PaymentEntity::GetpaymentModel()
+{
+    return PaymentModeId;
+}
+
+DateTime Entities::PaymentEntity::GetpaymentDate()
+{
+    return PaymentDate;
+}
+
+DateTime Entities::PaymentEntity::GetexpectedDate()
+{
+    return ExpectedDate;
+}
+
+String^ Entities::PaymentEntity::GetorderReference()
+{
+    return OrderReference;
+}
+
+float Entities::PaymentEntity::Getamount()
+{
+    return Amount;
+}
+
+void Entities::PaymentEntity::SetPaymentId(int paymentId)
+{
+    PaymentId = paymentId;
+}
+
+void Entities::PaymentEntity::SetExpectedDate(DateTime expectedDate)
+{
+    ExpectedDate = expectedDate;
+}
+
+void Entities::PaymentEntity::SetPaymentDate(DateTime paymentDate)
+{
+    PaymentDate =paymentDate;
+}
+
+void Entities::PaymentEntity::SetAmount(float amount)
+{
+    Amount = amount;
+}
+
+void Entities::PaymentEntity::SetPaymentModeId(int paymentModeId)
+{
+    PaymentModeId = paymentModeId;
+}
+
+void Entities::PaymentEntity::SetOrderReference(String^ orderReference)
+{
+    OrderReference = orderReference;
+}
+
---
+++ Components/enities/PaymentEntity.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+
+using namespace System;
+using namespace Data;
+
+namespace Entities
+{
+    public ref class PaymentEntity
+    {
+    protected:
+        int PaymentId;
+        int PaymentModeId;
+        String^ OrderReference;
+        float Amount;
+        DateTime PaymentDate;
+        DateTime ExpectedDate;
+    public:
+        PaymentEntity();
+        PaymentEntity(int paymentModeid, String^ orderReference, float amount, DateTime paymentDate,
+                      DateTime expectedDate);
+        PaymentEntity(int paymentId, int paymentModeid, String^ orderReference, float amount, DateTime paymentDate,
+                      DateTime expectedDate);
+        PaymentEntity(DataRow^ row);
+        int GetpaymentId();
+        int GetpaymentModel();
+        DateTime GetpaymentDate();
+        DateTime GetexpectedDate();
+        String^ GetorderReference();
+        float Getamount();
+        void SetPaymentId(int paymentId);
+        void SetExpectedDate(DateTime expectedDate);
+        void SetPaymentDate(DateTime paymentDate);
+        void SetAmount(float amount);
+        void SetPaymentModeId(int paymentModeId);
+        void SetOrderReference(String^ orderReference);
+    };
+}
