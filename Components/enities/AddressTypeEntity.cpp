2e3b44d530f94c287a0cd7e909fa54c8aae68e6f
Rooomain <rommor2810@gmail.com>
2022-12-02 10:02:32 +0100

    feat: addresstypeentity

---
+++ Components/enities/AddressTypeEntity.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include "AddressTypeEntity.h"
+
+Entities::AddressTypeEntity::AddressTypeEntity()
+{
+}
+
+Entities::AddressTypeEntity::AddressTypeEntity(String^ name): Name(name)
+{
+}
+
+Entities::AddressTypeEntity::AddressTypeEntity(int addressId, String^ name): AddressId(addressId), Name(name)
+{
+}
+
+Entities::AddressTypeEntity::AddressTypeEntity(DataRow^ row)
+{
+    this->AddressId = Convert::ToInt32(row["id"]);
+    this->Name = Convert::ToString(row["name"]);
+}
+
+int Entities::AddressTypeEntity::GetAddressId()
+{
+    return AddressId;
+}
+
+String^ Entities::AddressTypeEntity::GetName()
+{
+    return  Name;
+}
+
+void Entities::AddressTypeEntity::SetAddressId(int addressId)
+{
+    AddressId = addressId;
+}
+
+void Entities::AddressTypeEntity::setName(String^ name)
+{
+    Name = name;
+}
---
+++ Components/enities/AddressTypeEntity.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#pragma once
+
+using namespace System;
+using namespace Data;
+
+namespace Entities
+{
+    public ref class AddressTypeEntity
+    {
+    protected:
+        int AddressId;
+        String^ Name;
+    public:
+        AddressTypeEntity();
+        AddressTypeEntity(String^ name);
+        AddressTypeEntity(int addressId, String^ name);
+        AddressTypeEntity(DataRow^ row);
+        int GetAddressId();
+        String^ GetName();
+        void SetAddressId(int addressId);
+        void setName(String^ name);
+    };
+}
