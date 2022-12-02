901963c61bdd1e60088f1265662258f706b695ce
Rooomain <rommor2810@gmail.com>
2022-12-02 09:01:53 +0100

    feat: paymentmodeentity

---
+++ Components/enities/PaymentModeEntity.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include "PaymentModeEntity.h"
+
+Entities::PaymentModeEntity::PaymentModeEntity()
+{
+}
+
+Entities::PaymentModeEntity::PaymentModeEntity(String^ name): Name(name)
+{
+}
+
+Entities::PaymentModeEntity::PaymentModeEntity(int paymentModeId, String^ name): PaymentModeId(paymentModeId), Name(name)
+{
+}
+
+Entities::PaymentModeEntity::PaymentModeEntity(DataRow^ row)
+{
+    this->PaymentModeId = Convert::ToInt32(row["id"]);
+    this->Name = Convert::ToString(row["name"]);
+}
+
+int Entities::PaymentModeEntity::GetPaymentModeId()
+{
+    return PaymentModeId;
+}
+
+String^ Entities::PaymentModeEntity::GetName()
+{
+    return Name;
+}
+
+void Entities::PaymentModeEntity::SetPaymentModeId(int paymentModeId)
+{
+    PaymentModeId = paymentModeId;
+}
+
+void Entities::PaymentModeEntity::SetName(String^ name)
+{
+    Name = name;
+}
---
+++ Components/enities/PaymentModeEntity.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#pragma once
+
+using namespace System;
+using namespace Data;
+
+namespace Entities
+{
+    public ref class PaymentModeEntity
+    {
+    protected:
+        int PaymentModeId;
+        String^ Name;
+    public:
+        PaymentModeEntity();
+        PaymentModeEntity(String^ name);
+        PaymentModeEntity(int paymentModeId, String^ name);
+        PaymentModeEntity(DataRow^ row);
+        int GetPaymentModeId();
+        String^ GetName();
+        void SetPaymentModeId(int paymentModeId);
+        void SetName(String^ name);
+    };
+}
