9347a946bfa5ff13d6838122198f2107ea3b22cc
Antonin B <antonin.bourrel@viacesi.fr>
2022-12-02 09:50:50 +0100

    feat: CityEntity

---
+++ Components/enities/CityEntity.cpp
@@ -0,0 +1,51 @@
+#include "pch.h"
+#include "CityEntity.h"
+
+Entities::CityEntity::CityEntity()
+{
+}
+
+Entities::CityEntity::CityEntity(int zipCode, String^ city) : ZipCode(zipCode), City(city)
+{
+}
+
+Entities::CityEntity::CityEntity(int cityId, int zipCode, String^ city) : CityId(cityId), ZipCode(zipCode), City(city)
+{
+}
+
+Entities::CityEntity::CityEntity(DataRow^ row)
+{
+    this->CityId = Convert::ToInt32(row["id"]);
+    this->ZipCode = Convert::ToInt32(row["zipcode"]);
+    this->City = row["name"]->ToString();
+}
+
+int Entities::CityEntity::GetCityId()
+{
+    return CityId;
+}
+
+String^ Entities::CityEntity::GetCity()
+{
+    return City;
+}
+
+int Entities::CityEntity::GetZipCode()
+{
+    return ZipCode;
+}
+
+void Entities::CityEntity::SetCityId(int cityId)
+{
+    CityId = cityId;
+}
+
+void Entities::CityEntity::SetCity(String^ city)
+{
+    City = city;
+}
+
+void Entities::CityEntity::SetZipCode(int zipCode)
+{
+    ZipCode = zipCode;
+}
---
+++ Components/enities/CityEntity.h
@@ -0,0 +1,27 @@
+#pragma once
+
+
+using namespace System;
+using namespace Data;
+
+namespace Entities
+{
+    public ref class CityEntity
+    {
+    protected:
+        int CityId;
+        int ZipCode;
+        String^ City;
+    public:
+        CityEntity();
+        CityEntity(int zipCode, String^ city);
+        CityEntity(int cityId, int zipCode, String^ city);
+        CityEntity(DataRow^ row);
+        int GetCityId();
+        String^ GetCity();
+        int GetZipCode();
+        void SetCityId(int cityId);
+        void SetCity(String^ city);
+        void SetZipCode(int zipCode);
+    };
+}
