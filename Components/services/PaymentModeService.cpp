571669283f49ecd365bafa98f722b76ade68da35
Rooomain <rommor2810@gmail.com>
2022-12-05 17:07:19 +0100

    feat: PaymentModeService

---
+++ Components/services/PaymentModeService.cpp
@@ -0,0 +1,57 @@
+﻿#include "pch.h"
+#include "PaymentModeService.h"
+
+DataTable^ Services::PaymentModeService::GetPaymentModes()
+{
+    return dbProvider->ExecuteDataTable(
+"SELECT * FROM [management].[payment_modes]");
+}
+
+Entities::PaymentModeEntity^ Services::PaymentModeService::GetPaymentmode(int id)
+{
+    auto command = gcnew SqlClient::SqlCommand(
+        "SELECT * FROM [management].[payment_modes] WHERE [id] = @id");
+    command->Parameters->Add(gcnew SqlClient::SqlParameter("@id", id));
+    auto paymentMode  = dbProvider->ExecuteDataRow(command);
+    return gcnew Entities::PaymentModeEntity(paymentMode);
+}
+
+bool Services::PaymentModeService::AddPaymentMode(Entities::PaymentModeEntity^ paymentMode)
+{
+    auto command = gcnew SqlClient::SqlCommand(
+        "INSERT INTO [management].[payment_modes] (name) OUTPUT inserted.id VALUES (@name)");
+    command->Parameters->Add(gcnew SqlClient::SqlParameter("@name", paymentMode->GetName()));
+    return safe_cast<int>(dbProvider->ExecuteScalar(command));
+}
+
+bool Services::PaymentModeService::UpdatePaymentMode(Entities::PaymentModeEntity^ paymentMode)
+{
+    auto command = gcnew SqlClient::SqlCommand(
+        "UPDATE [management].[articles] SET name = @name WHERE id = @id");
+    command->Parameters->Add(gcnew SqlClient::SqlParameter("@name", paymentMode->GetName()));
+    return dbProvider->ExecuteNonQuery(command) == 1;
+}
+
+bool Services::PaymentModeService::DeletePaymentMode(int id)
+{
+    auto command = gcnew SqlClient::SqlCommand("DELETE FROM [management].[payment_modes] WHERE id =@id");
+    command->Parameters->Add(gcnew SqlClient::SqlParameter("@id", id));
+    return dbProvider->ExecuteNonQuery(command) == 1;
+}
+
+bool Services::PaymentModeService::DeletePaymentMode(Entities::PaymentModeEntity^ paymentMode)
+{
+    return DeletePaymentMode(paymentMode->GetPaymentModeId());
+}
+
+array<Entities::PaymentEntity^>^ Services::PaymentModeService::GetPayments(Entities::PaymentModeEntity^ payment)
+{
+    auto command = gcnew SqlClient::SqlCommand(
+    "SELECT * FROM [managements].[payment_modes] LEFT JOIN [managements].[payments] ON payment_modes.id = payments.payments_mode_id WHERE payment_modes.id = @id");
+    command->Parameters->Add(gcnew SqlClient::SqlParameter("@id", payment->GetPaymentModeId()));
+    auto payments = dbProvider->ExecuteDataTable(command);
+    auto result = gcnew array<Entities::PaymentEntity^>(payments->Rows->Count);
+    for (int i = 0; i < payments->Rows->Count; i++)
+        result[i] = gcnew Entities::PaymentEntity(payments->Rows[i]);
+    return result;
+}
---
+++ Components/services/PaymentModeService.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "AbstractService.h"
+#include "../entities/PaymentModeEntity.h"
+#include "../entities/PaymentEntity.h"
+
+using namespace System;
+using namespace Data;
+
+namespace Services
+{
+    public ref class PaymentModeService : public AbstractService
+    {
+    public:
+        DataTable^ GetPaymentModes();
+        Entities::PaymentModeEntity^ GetPaymentmode(int id);
+        bool AddPaymentMode (Entities::PaymentModeEntity^ paymentMode);
+        bool UpdatePaymentMode(Entities::PaymentModeEntity^ paymentMode);
+        bool DeletePaymentMode(int id);
+        bool DeletePaymentMode(Entities::PaymentModeEntity^ paymentMode);
+        array<Entities::PaymentEntity^>^ GetPayments(Entities::PaymentModeEntity^ payment);
+    };
+}
